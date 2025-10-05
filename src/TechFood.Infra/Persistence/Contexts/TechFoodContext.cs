314dac60dd9bf88809389ebc562310b96a602b34
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-10-05 20:50:17 -0300

    feat: implement entity configurations for User and value objects in EF Core

--- src/TechFood.Infra/Persistence/Contexts/TechFoodContext.cs
+++ src/TechFood.Infra/Persistence/Contexts/TechFoodContext.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TechFood.Domain.Common.Entities;
 using TechFood.Domain.Entities;
 
 namespace TechFood.Infra.Persistence.Contexts;
@@ -24,6 +27,79 @@ public class TechFoodContext(DbContextOptions<TechFoodContext> options) : DbCont
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TechFoodContext).Assembly);
+
+        var properties = modelBuilder.Model
+            .GetEntityTypes()
+            .SelectMany(t => t.GetProperties());
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(Entity).IsAssignableFrom(entityType.ClrType))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "e");
+                var property = Expression.Property(parameter, nameof(Entity.IsDeleted));
+                var condition = Expression.Equal(property, Expression.Constant(false));
+                var lambda = Expression.Lambda(condition, parameter);
+
+                modelBuilder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+
+                modelBuilder.Entity(entityType.ClrType)
+                    .HasKey(nameof(Entity.Id));
+
+                modelBuilder.Entity(entityType.ClrType)
+                    .Property(nameof(Entity.Id))
+                    .IsRequired()
+                    .ValueGeneratedNever();
+            }
+        }
+
+        var stringProperties = properties.Where(p => p.ClrType == typeof(string));
+        foreach (var property in stringProperties)
+        {
+            var maxLength = property.GetMaxLength() ?? 50;
+
+            property.SetColumnType($"varchar({maxLength})");
+        }
+
+        var booleanProperties = properties
+            .Where(p => p.ClrType == typeof(bool) ||
+                        p.ClrType == typeof(bool?));
+
+        foreach (var property in booleanProperties)
+        {
+            property.SetColumnType("bit");
+            property.IsNullable = false;
+        }
+
+        var dateTimeProperties = properties.Where(p => p.ClrType == typeof(DateTime));
+
+        foreach (var property in dateTimeProperties)
+        {
+            property.SetColumnType("datetime");
+        }
+
+        var enumProperties = properties.Where(p => p.ClrType == typeof(Enum));
+
+        foreach (var property in enumProperties)
+        {
+            property.SetColumnType("smallint");
+        }
+
+        var amountProperties = properties
+            .Where(p => p.ClrType == typeof(decimal) ||
+                        p.ClrType == typeof(decimal?));
+
+        foreach (var property in amountProperties)
+        {
+            property.SetColumnType("decimal(6, 2)");
+        }
+
+        base.OnModelCreating(modelBuilder);
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
