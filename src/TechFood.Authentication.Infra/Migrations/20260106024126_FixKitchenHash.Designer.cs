18749af8a47688877056a90512eb8fba7ab3acbd
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2026-01-05 23:42:12 -0300

    Update kitchen-service client secret hash and migration
    Updated the seeded ClientSecretHash for kitchen-service in the authentication database. Added migration 20260106024126_FixKitchenHash to update the hash in the ServiceClients table, with rollback support. Migration designer reflects the new hash in model data seeding.

--- src/TechFood.Authentication.Infra/Migrations/20260106024126_FixKitchenHash.Designer.cs
+++ src/TechFood.Authentication.Infra/Migrations/20260106024126_FixKitchenHash.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechFood.Authentication.Infra.Persistence.Contexts;
 
@@ -11,9 +12,11 @@ using TechFood.Authentication.Infra.Persistence.Contexts;
 namespace TechFood.Authentication.Infra.Migrations
 {
     [DbContext(typeof(AuthContext))]
-    partial class AuthContextModelSnapshot : ModelSnapshot
+    [Migration("20260106024126_FixKitchenHash")]
+    partial class FixKitchenHash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
